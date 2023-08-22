0c8b840c5333f69ff9643e7c6e3771be86b8e84d
alszero <alszero05@gmail.com>
2023-08-23 06:12:25 +0900

    home page

--- src/pages/hompage/home.js
+++ src/pages/hompage/home.js
@@ -12,6 +12,17 @@ const Home = ()=> {
             <Header navigate={navigate}/>
             <div>
                 <img className="bg" src={BackgroundImg} alt="bg" />
+                <div className="title">
+                    <p className="first">USER에서 MAKER로 거듭나고자 하는</p>
+                    <p className="first" style={{marginTop : '1%'}}>당신을 위한 최고의 선택, APP&ME</p>
+                    <hr />
+                    <p className="sub">앱앤미와 관련되어 궁금한 점이 있다면</p>
+                    <p className="sub">에스크와 인스타를 통해 질문해주세요!</p>
+                    <p className="date">03.15-03.19</p>
+                    <div onClick={()=>{navigate("/apply")}} className="apply" style={{ cursor: 'pointer', fontWeight: 'bold' }}>
+                        지원하기
+                    </div>
+                </div>
             </div>
         </div>
     );
