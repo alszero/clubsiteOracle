c7807741b475b5e47bdee7244f4f3e87b7673028
alszero <alszero05@gmail.com>
2023-08-23 06:13:16 +0900

    logo img 추가 및 header 변경

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/hompage/home';
+import JOIN from './pages/join/join';
+import LOGIN from './pages/login/login';
+import MyPage from './pages/mypage/mypage';
+import ABOUT from './pages/about/about';
 
 function App() {
   return (
@@ -8,6 +12,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}/>
+          <Route path="/join" element={<JOIN />}/>
+          <Route path="/mypage" element={<MyPage />}/>
+          <Route path="/login" element={<LOGIN />}/>
+          <Route path="/about" element={<ABOUT />}/>
         </Routes>
       </BrowserRouter>
     </div>
