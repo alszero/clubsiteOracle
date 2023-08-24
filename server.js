83d5aa6c91922489555ffa1fdff116585fb33e07
alszero05 <alszero05@gmail.com>
2023-08-24 13:20:09 +0900

    server 수정

--- server.js
+++ server.js
@@ -8,16 +8,6 @@ app.use(express.json());
 const cors = require('cors');
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, '/src')));
-
-app.get('/', (res, req) => {
-  req.sendFile(path.join(__dirname, '/src/App.js'));
-});
-
-app.get('*', (res, req) => {
-  req.sendFile(path.join(__dirname, '/src/App.js'));
-});
-
 http.listen(5000, () => {
   console.log("Listening on 5000");
 });
